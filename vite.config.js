d4edf2a30913404c9e76fc3c9fdd264ad0a3bc56
Yamach1ta <gustaplay345@gmail.com>
2025-06-12 04:44:18 -0300

    Salvar progresso antes de puxar remoto

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/arquivo-confidencial/', //versao-beta-da-perfei-o
+  base: '/versao-beta-da-perfei-o/', //versao-beta-da-perfei-o
   plugins: [react()],
 })
